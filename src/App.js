8bd2b9857c4c82eda7e1d1021cd60c72e793b004
network-Error <iamimew@gmail.com>
2024-02-21 17:26:20 +0300

    init: start task

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TabsTest from './components/tabs-test';
+
+function App() {
+  return (
+    <div className="App">
+      <TabsTest />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/tabs-test.jsx
@@ -0,0 +1,33 @@
+import Tabs from "./Tabs"
+import './styles.css'
+
+function RandomComponent() {
+  return <h1>Some random content</h1>
+}
+
+const TabsTest = () => {
+  const tabs = [
+    {
+      label: 'Tab 1',
+      content: <div>This is content for Tab 1</div>
+    },
+    {
+      label: 'Tab 2',
+      content: <div>This is content for Tab 2</div>
+    },
+    {
+      label: 'Tab 3',
+      content: <RandomComponent />
+    }
+  ]
+
+  function handleChange(currentTabIndex) {
+    console.log(currentTabIndex);
+  }
+
+  return (
+    <Tabs tabsContent={tabs} onChange={handleChange} />
+  )
+}
+
+export default TabsTest
